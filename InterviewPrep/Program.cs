0bc0c0c46c8e48be9383047c7713346701ee4832
Joseph Callahan <Joseph.callahan1@outlook.com>
2018-12-04 23:05:43 -0500

    Reverse a string

--- InterviewPrep/Program.cs
+++ InterviewPrep/Program.cs
@@ -10,6 +10,41 @@ namespace InterviewPrep
     {
         static void Main(string[] args)
         {
+            string s = "my.name.is.joe";
+            StringReverse(s);
+        }
+        public static void StringReverse(string s)
+        {
+            int j = 0;
+            int len = 0;
+            Stack<string> stack = new Stack<string>();
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] != '.')
+                {
+                    len++;
+                    if (i == s.Length - 1)
+                    {
+                        stack.Push(s.Substring(j, len));
+                    }
+                }
+                
+                else
+                {
+                    stack.Push(s.Substring(j, len));
+                    if (i != s.Length - 1)
+                    {
+                        j = i + 1;
+                    }
+
+                    len = 0;
+                }
+            }
+            foreach (var item in stack)
+            {
+                
+                Console.Write(item + ".");
+            }
         }
     }
 }
